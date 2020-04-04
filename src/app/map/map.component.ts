46cc6263474bf586ae900163c28629741e89cade
Mike van Niel <mike.van.niel@gmail.com>
2020-04-04 09:39:11 +0200

    WIP: added a header, removed not used marker stuff

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,22 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
-import { icon, Marker } from 'leaflet';
 import { MarkerService } from '../marker.service';
-
-const iconRetinaUrl = 'assets/marker-icon-2x.png';
-const iconUrl = 'assets/marker-icon.png';
-const shadowUrl = 'assets/marker-shadow.png';
-const iconDefault = L.icon({
-  iconRetinaUrl,
-  iconUrl,
-  shadowUrl,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
-});
-L.Marker.prototype.options.icon = iconDefault;
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -60,12 +44,6 @@ export class MapComponent implements OnInit {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
-  }
-
-  private addMyCurrentLocationMarker(): void {
-    // add a marker with label Me
-      //var myMarker = L.marker([this._global.curPosLat, this._global.curPosLng]).addTo(this.map);
-      //myMarker.bindPopup("<b>Me</b>").openPopup();
   }
 
 }
