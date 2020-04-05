d66b60d46b4c0ea650b5902b4beb552d73643648
Mike van Niel <mike.van.niel@gmail.com>
2020-04-05 13:08:18 +0200

    applied nearest weather station to home component, added source text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,78 @@ import { Chart } from 'angular-highcharts';
 import { FormControl } from '@angular/forms';
 import { PlotLineOrBand } from 'highcharts';
 
+var wstations = [
+  { "name": "Rotterdam",          "code": "06344", "coordinate": [4.45, 51.95] },
+  { "name": "Woensdrecht",        "code": "06340", "coordinate": [4.333, 51.45] },
+  { "name": "Amsterdam/Schiphol", "code": "06240", "coordinate": [4.767, 52.3] },
+  { "name": "De Kooy",            "code": "06235", "coordinate": [4.783, 52.917] },
+  { "name": "Hoorn/Terschelling", "code": "06251", "coordinate": [5.35, 53.383] },
+  { "name": "Lelystad",           "code": "06269", "coordinate": [5.533, 52.45] },
+  { "name": "De Bilt",            "code": "06260", "coordinate": [5.183, 52.1] },
+  { "name": "Leeuwarden",         "code": "06270", "coordinate": [5.767, 53.217] },
+  { "name": "Deelen",             "code": "06275", "coordinate": [5.883, 52.067] },
+  { "name": "Hoogeveen",          "code": "06279", "coordinate": [6.517, 52.733] },
+  { "name": "Groningen",          "code": "06280", "coordinate": [6.583, 53.133] },
+  { "name": "Hupsel",             "code": "06283", "coordinate": [6.65, 52.067] },
+  { "name": "Heino",              "code": "06278", "coordinate": [6.267, 52.433] },
+  { "name": "Vlissingen",         "code": "06310", "coordinate": [3.6, 51.45] },
+  { "name": "Gilze Rijen",        "code": "06350", "coordinate": [4.933, 51.567] },
+  { "name": "Eindhoven",          "code": "06370", "coordinate": [5.417, 51.45] },
+  { "name": "Volkel",             "code": "06375", "coordinate": [5.7, 51.65] },
+  { "name": "Maastricht",         "code": "06380", "coordinate": [5.783, 50.917] },
+  { "name": "Arcen",              "code": "06391", "coordinate": [6.2, 51.5] }
+];
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::                                                                         :::
+//:::  This routine calculates the distance between two points (given the     :::
+//:::  latitude/longitude of those points). It is being used to calculate     :::
+//:::  the distance between two locations using GeoDataSource (TM) prodducts  :::
+//:::                                                                         :::
+//:::  Definitions:                                                           :::
+//:::    South latitudes are negative, east longitudes are positive           :::
+//:::                                                                         :::
+//:::  Passed to function:                                                    :::
+//:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
+//:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
+//:::    unit = the unit you desire for results                               :::
+//:::           where: 'M' is statute miles (default)                         :::
+//:::                  'K' is kilometers                                      :::
+//:::                  'N' is nautical miles                                  :::
+//:::                                                                         :::
+//:::  Worldwide cities and other features databases with latitude longitude  :::
+//:::  are available at https://www.geodatasource.com                         :::
+//:::                                                                         :::
+//:::  For enquiries, please contact sales@geodatasource.com                  :::
+//:::                                                                         :::
+//:::  Official Web site: https://www.geodatasource.com                       :::
+//:::                                                                         :::
+//:::               GeoDataSource.com (C) All Rights Reserved 2018            :::
+//:::                                                                         :::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+function distance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		if (unit=="N") { dist = dist * 0.8684 }
+		return dist;
+	}
+}
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -115,11 +187,16 @@ export class HomeComponent implements OnInit {
     }]
   });
 
+  // My current position (determined by browser on the device)
+  latMe: number;
+  lonMe: number;
+  nearestStationCode: string = '06344';
+  nearestStationName: string = 'Rotterdam';
+
   constructor(private mainService: MainService) { }
 
   ngOnInit(): void {
     this.getCurrentLocation();
-    this.getZapData();
   }
 
   applyZapChance(zapValue: number): void {
@@ -136,17 +213,36 @@ export class HomeComponent implements OnInit {
   resetZapChance(): void {
     this.getZapData();
   }
+
   getCurrentLocation(): void {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position: Position) => {
             if (position) {
-              // try to determine a nearby weather station 
-                //this.curPosLat = position.coords.latitude;
-                //this.curPosLng = position.coords.longitude;
+              this.latMe = position.coords.latitude;
+              this.lonMe = position.coords.longitude;
+              this.determineNearestWeatherStation();
+              return;
             }
         })
     }
+    this.getZapData();
   }
+
+  determineNearestWeatherStation(): void {
+    let shortestDistance: number = 0;
+    for (const wstation of wstations) {
+      const latStation = wstation.coordinate[1];
+      const lonStation = wstation.coordinate[0];
+      let distanceBetweenMeAndStation: number = distance(this.latMe, this.lonMe, latStation, lonStation, 'K');
+      if (shortestDistance == 0 || distanceBetweenMeAndStation < shortestDistance) {
+        shortestDistance = distanceBetweenMeAndStation;
+        this.nearestStationCode = wstation.code;
+        this.nearestStationName = wstation.name;
+      }
+    }
+    this.getZapData();
+  }
+
   getZapData(): void {
     this.mainService.getTimeSeriesData({
       'Readers': [
@@ -159,7 +255,7 @@ export class HomeComponent implements OnInit {
               'TMP', 'RH'
             ],
             'LocationCodes': [
-              '06344'
+              this.nearestStationCode
             ]
           }
         }
